91f694f82f7547173d52b343956c28ccfd43e6f1
Keyboard1000n17 <keyboard1000n17@gmail.com>
2026-06-04 23:37:05 +0300

    Add convenience scripts for dev, waybar to package list

---
+++ setup-dev.sh
@@ -0,0 +1,2 @@
+mkdir -p ../amethystOS-ISO/
+qemu-img create -f raw amethyst.img 5G
---
+++ start-iso.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+qemu-system-x86_64 \
+  -hda ../amethystOS-ISO/amethyst.img \
+  -cdrom ../amethystOS-ISO/amethystos-$(date +%Y.%m.%d)-x86_64.iso \
+  -m 4G \
+  -accel kvm \
+  -monitor stdio
