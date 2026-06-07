ec39fc7bc176eb0aa5fdbe950adfd71f9b83a220
Keyboard1000n17 <keyboard1000n17@gmail.com>
2026-06-07 22:35:02 +0300

    Add wallpaper

--- generate-iso.sh
+++ generate-iso.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 if [[ -d ../amethystOS-ISO/ ]]; then
+  cp -r ./branding/ ./airootfs/usr/share/amethystos/
+  export MKARCHISO_MKSQUASHFS_OPTIONS="-comp xz -processors 4"
   mkarchiso -v -w /tmp/archiso/ -o ../amethystOS-ISO/ .
 else
   echo "Please run ./setup-dev.sh!"
